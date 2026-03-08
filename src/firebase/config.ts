fcf7053a37b4ecdbee1f478519debb6ce06509b7
Hugues R <hugues.rabier@gmail.com>
2026-03-08 15:04:54 +0000

    Assure toi qu'aucune clé API ne sera visible quand je vais pousser le pr

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,7 @@
-// @ts-nocheck
-// It's not recommended to store API keys in your source code.
-// This is just for demonstration purposes.
 export const firebaseConfig = {
-  apiKey: 'AIzaSyAJqPotia_9s4SgzSSTyes2D4dY6ScDSQE',
-  authDomain: 'suivi-alim-2025.firebaseapp.com',
-  projectId: 'suivi-alim-2025',
-  messagingSenderId: '699348141442',
-  appId: '1:699348141442:web:e575323c137b170beefb9b',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
